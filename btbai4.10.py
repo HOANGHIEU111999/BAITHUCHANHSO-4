5d9bf6354a846b2f0f60b1d047b471321f7cd8f4
HOANGHIEU111999 <50811677+HOANGHIEU111999@users.noreply.github.com>
2019-05-20 07:42:26 +0700

    Add files via upload

---
+++ btbai4.10.py
@@ -0,0 +1,5 @@
+#Cắt list: lấy list nhưng bỏ phần đầu và cuối
+ds = input("Nhap chuoi: ").split()
+x = ds[1:-1]
+for c in x:
+    print("Cắt list: ",x)
---
+++ btbai4.13.py
@@ -0,0 +1,6 @@
+#Sắp xếp các phần tử trong list
+#Sắp xếp các phần tử trong list
+ds = input("Nhap chuoi: ").split()
+ds.sort()
+for ch in ds:
+    print("Sap xep cac phan tu: ",ch)
